950df5d6bf44407b4b3c1b34ba6aad28583e64df
Jesse Woolsey <jesse232@live.com>
2020-01-13 10:28:03 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,7 @@
+student_1 = "Harry Potter"
+student_2 = "Ron Weasley" 
+student_3 = "Hermione Granger"
+student_4 = "Draco Malfoy"
+
+students = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Draco Malfoy"][0]
+
